b07f56f111430580f92fd9963025c8aaea84f40f
olima284 <olivialima.student@careerdevs.com>
2022-04-26 16:00:00 -0400

    Adding HelloInternet2 to Github

---
+++ src/main/java/com/careedevs/helloInternet02/HelloInternet02Application.java
@@ -0,0 +1,13 @@
+package com.careedevs.helloInternet02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInternet02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInternet02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/careedevs/helloInternet02/HelloInternet02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careedevs.helloInternet02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInternet02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
